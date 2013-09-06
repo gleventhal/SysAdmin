628778cdc8835c656bc4c0d927df176a163218b5
gleventhal <gregglev@yahoo.com>
2013-09-06 11:41:26 -0400

    fib sequence in perl, just for fun

---
+++ fibonacci.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl 
+my $count = 1; my $a = 0; my $b = 1;
+print "$a\n";
+print "$b\n";
+while ( $count < 50 ) {
+$sum = $a + $b;
+print "$sum\n";
+$a = $b;
+$b = $sum;
+$count += 1;
+done;
+}'
