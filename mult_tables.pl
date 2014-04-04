33cb299b42287353efcf55ba904707b09432ad9a
gleventhal <gregglev@yahoo.com>
2014-04-04 15:03:59 -0400

    Added comment

--- mult_tables.pl
+++ mult_tables.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 # Multiplication Table problem
+# This creates a 12 x 12 multiplication table with hypen delmiters.  I got the idea from an article about interview questions for DevOps guys.
 
 for($i=1;$i<=12;$i++){ 
 foreach(1..12) {
