650a46d8156d4e2002fe7da1f5a198c853eba4b2
gleventhal <gregglev@yahoo.com>
2014-04-04 15:01:41 -0400

    Removed uneeded done statement from While loop and indented.

--- fibonacci.pl
+++ fibonacci.pl
@@ -3,10 +3,9 @@ my $count = 1; my $a = 0; my $b = 1;
 print "$a\n";
 print "$b\n";
 while ( $count < 50 ) {
-$sum = $a + $b;
-print "$sum\n";
-$a = $b;
-$b = $sum;
-$count += 1;
-done;
+	$sum = $a + $b;
+	print "$sum\n";
+	$a = $b;
+	$b = $sum;
+	$count += 1;
 }
