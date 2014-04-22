14aecde639f8a05a5a543d8c78bf958d4a7e7cd4
gleventhal <gregglev@yahoo.com>
2014-04-22 18:23:44 -0400

    Bash Fizzbuzz

---
+++ bash_fizzbuzz.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# FizzBuzz in Bash in Unremarkable Fashion!
+for i in {1..100}
+do ( (($i % 15 == 0)) && echo "FizzBuzz")||\
+	( (($i % 5 == 0 )) && echo "Buzz") ||\
+	( (($i % 3 == 0 )) && echo "Fizz") ||\
+	echo "$i"
+done
