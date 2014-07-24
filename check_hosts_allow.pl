a24ad709cf2c881ec103163e0e7334d1b2224e49
gleventhal <gregglev@yahoo.com>
2014-07-24 11:55:27 -0400

    Added main subroutine

--- check_hosts_allow.pl
+++ check_hosts_allow.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 ###########################################################################################
 #               Gregg Leventhal for Weill Cornell Medical College 2014                    #
-#																						  #
+#											  #
 # Takes a single IP Address as an argument, and ensures that it is allowed in hosts.allow #
 ###########################################################################################
 
@@ -9,28 +9,8 @@ my $IP = $ARGV[0];
 my $FILE = '/etc/hosts.allow';
 my $TEMPFILE = '/etc/hosts.allow.TEMP';
 
-sub make_changes
+sub main
 {
-    rename $TEMPFILE, $FILE;   		
-    print "$IP has been added to $FILE\n";
-    close $FH;
-    if( -e "$TEMPFILE") {
-	unlink "$TEMPFILE";
-    }   
-}
-
-# Are we root/running with sudo?
-if( $> != 0 ) {
-    die "\n$0 must be run as root or using sudo!\n\n";
-}
-
-# Make sure it is an IP with either 157. or 140. as first octet
-unless ( $IP =~ qr/^(140|157)\.(\d{1,3}\.){2}\d{1,3}$/ ) {	
-    die "Usage: $0 IP Address\n";
-} 
-else 
-{		
-
     open FH2, ">", $TEMPFILE;
     open (FH, "<", "$FILE");    	
     
@@ -55,5 +35,28 @@ else
     }
   } 
 }
+}
 
+sub make_changes
+{
+    rename $TEMPFILE, $FILE;   		
+    print "$IP has been added to $FILE\n";
+    close $FH;
+    if( -e "$TEMPFILE") {
+	unlink "$TEMPFILE";
+    }   
+}
+
+# Are we root/running with sudo?
+if( $> != 0 ) {
+    die "\n$0 must be run as root or using sudo!\n\n";
+}
+
+# Make sure it is an IP with either 157. or 140. as first octet
+unless ( $IP =~ qr/^(140|157)\.(\d{1,3}\.){2}\d{1,3}$/ ) {	
+    die "Usage: $0 IP Address\n";
+} 
+else 
+{		
+main
 make_changes
