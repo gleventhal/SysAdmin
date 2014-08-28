47fe2102fdeb6032caca6ce673c14859224c9fbe
gleventhal <gregglev@yahoo.com>
2014-08-27 22:02:40 -0400

    Added a faster Perl Tail

---
+++ tail_perl_2.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+$FILE = $ARGV[0] || die "Usage: $0 File\n";
+@LINES = ();
+open(FH, $FILE);
+
+  foreach $line (<FH>) {
+    push @LINES, $line;
+  }
+
+$LENGTH = @LINES;
+
+foreach (($LENGTH - 10 )..$LENGTH) {
+  print $LINES[$_], "\n";
+}
