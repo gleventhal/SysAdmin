3974eacf384c60ed5b7e9986bb0acfc811dc6662
Gregg Leventhal <gregg@admins-mac-mini.home>
2015-04-04 11:25:00 -0400

    For felix

--- matchParens.py
+++ matchParens.py
@@ -2,6 +2,7 @@
 # For Felix
 
 def matchParens(pstring):
+    '''Check string of parenthesis for sane groupuing and nesting'''
     plist = [l for l in pstring]
     newlist = []
     if plist[0] == ')' or plist.count('(') != plist.count(')') or plist[-1::] == '(':
@@ -19,7 +20,21 @@ def matchParens(pstring):
             return False
     return True
 
+# Check Function
 assert matchParens('((()))') == True
 assert matchParens('(()(())())') == True
 assert matchParens('()()(') == False
 assert matchParens('(()(())()') == False
+assert matchParens('(()())()((()))') == True
+
+def elementMatch(a1, a2):
+    '''Find first similar element in a pair of sorted arrays'''
+    try:
+        return sorted(set(a1) & set(a2))[0]
+    except IndexError:
+        return False
+
+# Check function
+assert elementMatch([2, 3, 5, 7, 8, 23], [4, 6, 8, 15, 23, 34]) == 8
+assert elementMatch([12, 13, 25, 27, 28, 123], [24, 26, 28, 215, 123, 134]) == 28
+assert elementMatch([12, 13, 25, 27, 28, 223], [124, 126, 128, 215, 123, 134]) == False # No Match
