85b5b8115f1482deaec483f0bbea77a03899b217
Gregg Leventhal <gregg@admins-mac-mini.home>
2015-04-19 11:34:30 -0400

    Added second algo

--- uniquestr.py
+++ uniquestr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Dumb interview Questions :)
 # Find the first unique char in a string (from left to right)
+import timeit
+
 def ustr(n):
     matches = {}
     for i in list(n):
@@ -9,3 +11,21 @@ def ustr(n):
         else:
             matches[i] = 1
     return n[(min(map(lambda x: n.index(x), [i for i in matches.keys() if matches[i] == 1])))]
+
+# Better Algo
+def ustring(s):
+    slist = list(s)
+    for i in slist:
+        if i in slist[slist.index(i) + 1::]:
+            next
+        else:
+            return i
+
+
+timeit.timeit("ustr('pzaramoumpteruotle')",setup="from __main__ import ustr")
+timeit.timeit("ustr('pzaramoumpteruotle')",setup="from __main__ import ustr" )
+timeit.timeit("ustr('pzaramoumpteruotle')", setup="from __main__ import ustr" )
+timeit.timeit("ustring('pzaramoumpteruotle')",setup="from __main__ import  ustring")
+timeit.timeit("ustring('pzaramoumpteruotle')",setup="from __main__ import ustring" )
+timeit.timeit("ustring('pzaramoumpteruotle')",setup="from __main__ import ustring" )
+
