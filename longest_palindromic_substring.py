92bd0379bf4d6d1d4752f0ce487306f09179ae91
Gregg Leventhal <gregg@Greggs-MBP.home>
2019-11-16 19:24:51 -0500

    finished

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -5,7 +5,8 @@
 from collections import defaultdict
 
 def longest_palindromic_substring(s):
-    pals = []
+    longest = s[0]
+    idx = 0
     pairs = defaultdict(list)
     for i in range(len(s)):
         pairs[s[i]].append(i)
@@ -19,8 +20,17 @@ def longest_palindromic_substring(s):
                     l = p[lo]
                     h = p[hi]+1
                     if s[l:h] == s[l:h][::-1] and len(s[l:h]) > 1:
-                        pals.append(s[l:h])
+                        if len(s[l:h]) > len(longest):
+                            longest = s[l:h]
+                            idx = l
+                        elif len(s[l:h]) == len(longest):
+                        # Since dict is unordered, if we have multiple
+                        # matches of the same length, use the one with the
+                        # lowest starting index (left-most)
+                            if l < idx:
+                                longest = s[l:h]
+                                idx = l
                     hi -= 1
                 hi = len(p) - 1
                 lo += 1
-    return max(pals, key=lambda x: len(x))
+    return longest
